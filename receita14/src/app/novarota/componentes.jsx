a8ef545e727cf137fd01031ee0a08f66f6f03887
Marlison Silva <marlisonsoaresjp@gmail.com>
2025-01-08 14:00:10 -0300

    receita 14 OK

--- receita14/src/app/novarota/componentes.jsx
+++ receita14/src/app/novarota/componentes.jsx
@@ -3,3 +3,15 @@ export function MariaPrea() {
         <h2>Morreu Maria Preá...</h2>
     )
 }
+
+export function JoaoPrea() {
+    return (
+        <h2>Morreu João Preá...</h2>
+    )
+}
+
+export function Mensagem(props) {
+    return (
+        <h2>{props.mensagem}</h2>
+    )
+}
